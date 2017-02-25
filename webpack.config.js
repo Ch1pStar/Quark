7c493286c812d270d055bbc67164ccdedc54678b
Nedelin Yordanov <nedeliny@gmail.com>
2017-02-25 10:51:31 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path      = require('path');
+const fs        = require('fs');
+const pkg       = require('./package.json');
+
+
+const config = { pkg };
+
+module.exports = {
+  entry: __dirname+'/src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    filename: "quark.js"
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: [
+              'babel-loader?cacheDirectory=true&presets[]=es2015&plugins[]=transform-class-properties',
+              `preprocess?${JSON.stringify(config)}`,
+          ],
+        },
+        {
+            test: /\.(glsl|frag|vert)$/,
+            exclude: /node_modules/,
+            loaders: ['raw', 'glslify'],
+        },
+    ]
+  }
+};
